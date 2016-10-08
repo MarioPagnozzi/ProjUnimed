999ef8c97cdfa5d7ca280733ab353272452dc3de
MarioPagnozzi <mario.antonio.pagnozzi@creci.org.br>
2016-10-08 16:20:04 -0300

    arquivos modificados

---
+++ SisUnimed/ItemDePagina.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SisUnimed
+{
+    public class ItemDePagina
+    {
+        public String Controller { get; set; }
+        public String Action { get; set; }
+        public int ConteudoId { get; set; }
+    }
+}
